0e361c3df43895641e5bf8ddfc29d0ecc3a263ff
Lucas G-R <lucasgether@gmail.com>
2025-02-05 17:59:58 +0100

    Add student_service and update controller

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -3,21 +3,21 @@ import six
 
 from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
+from swagger_server.service.student_service import *
 
 
-def add_student(body=None):  # noqa: E501
+
+def add_student(body=None): # noqa: E501
     """Add a new student
-
     Adds an item to the system # noqa: E501
-
     :param body: Student item to add
     :type body: dict | bytes
-
-    :rtype: str
+    :rtype: float
     """
     if connexion.request.is_json:
-        body = Student.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'Student added!'
+        body = Student.from_dict(connexion.request.get_json()) # noqa: E501
+        return add(body)
+    return 500, 'Couldn\'t add student'
 
 
 def delete_student(student_id):  # noqa: E501
@@ -30,7 +30,9 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: object
     """
-    return 'Student deleted!'
+    if connexion.request.is_json:
+        return delete(student_id)
+    return 500, 'Couldn\'t delete student'
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -43,4 +45,6 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 'do some magic!'
+    if connexion.request.is_json:
+        return get_by_id(student_id)
+    return 500, 'Couldn\'t get student'
