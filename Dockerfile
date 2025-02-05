a23fec6011e202e90c9843fde65fdecb029c9200
Lucas G-R <lucasgether@gmail.com>
2025-02-05 23:30:19 +0100

    Working MongoDB integration

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
-FROM python:3.6-alpine
+FROM python:3.8-alpine
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
 COPY requirements.txt /usr/src/app/
 
+RUN pip install --upgrade pip
+
 RUN pip3 install --no-cache-dir -r requirements.txt
 
 COPY . /usr/src/app
+
+EXPOSE 27017
 
 EXPOSE 8080
 
--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -2,30 +2,33 @@ import os
 import tempfile
 from functools import reduce
 
-from tinydb import TinyDB, Query
+# from tinydb import TinyDB, Query
+from pymongo import MongoClient
 
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+# TODO: delete
+# db_dir_path = tempfile.gettempdir()
+# db_file_path = os.path.join(db_dir_path, "students.json")
+# student_db = TinyDB(db_file_path)
 
+mongo_db_client = MongoClient("mongodb://mongo:27017")
+student_db = mongo_db_client["student_database"]
+student_collection = student_db["students"]
 
 def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
+    res = student_collection.find_one({
+        "first_name": student.first_name,
+        "last_name": student.last_name
+    })
     if res:
         return 'already exists', 409
 
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
+    doc_id = student_collection.insert_one(student.to_dict())
+    # student.student_id = int(str(doc_id.inserted_id))
     return student.student_id
 
 
 def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = student_collection.find_one({"student_id": int(student_id)})
     if not student:
         return 'not found', 404
     student['student_id'] = student_id
@@ -34,8 +37,8 @@ def get_by_id(student_id=None, subject=None):
 
 
 def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = student_collection.find_one({"student_id": int(student_id)})
     if not student:
         return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
+    student_collection.delete_one(student)
     return student_id
